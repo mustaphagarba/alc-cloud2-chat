e0d5b70e452ef3e663760fb3bd42eef5988502e6
Mustapha Garba <mgarba88@gmail.com>
2019-10-18 21:00:40 +0100

    1 hour commit. Currently working on store.js

--- src/Store.js
+++ src/Store.js
@@ -4,9 +4,43 @@ import { checkPropTypes } from 'prop-types';
 
 const CTX = React.createContext();
 
+/*
+    msg { 
+        from:   'user'
+        msg:    'hi'
+        topic:  'general'
+
+
+   }
+
+   state    {
+        general:     [
+            {msg},  {msg},  {msg}, {newmsg}
+        ] 
+        topic2:
+
+   }
+
+*/
+
 function reducer(state, action){
+    const {from, msg, topic} = action.payload;
     switch(action.type){
-        
+        case 'RECEIVE MESSAGE' :
+            return {
+                ...state,
+                [action.payload.topic]:     {
+                    ...state[topic].
+                    {
+                        from:   action.payload.from,
+                        msg:    action.payload.msg
+
+                    }
+                }
+
+            }
+        default :
+            return state 
     }
 }
 
